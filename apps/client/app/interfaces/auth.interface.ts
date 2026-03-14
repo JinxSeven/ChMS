841a70f070d6f479c18d8ae45f64fd3203d0e027
Samuel Livingston <164835921+JinxSeven@users.noreply.github.com>
2026-03-14 14:29:06 +0530

    Added SignUpFormData

--- apps/client/app/interfaces/auth.interface.ts
+++ apps/client/app/interfaces/auth.interface.ts
@@ -3,3 +3,12 @@ export interface AuthState {
   loading: boolean;
   error: string | null;
 }
+
+export interface SignUpFormData {
+  email: string;
+  fullname: string;
+  userRole: UserRole;
+  password: string;
+}
+
+export type UserRole = 'user' | 'admin' | 'superAdmin'
